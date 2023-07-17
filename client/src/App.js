9b46b145b56424a24da377db76e2f813ef80e49c
agamswaroop619 <agam.swaroop619@gmail.com>
2023-07-17 21:58:03 +0530

    frontend update 1/5

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <div className="font-mono w-[100%] h-[100vh] bg-blue-400">
-      <div>
-      <h1>Shareify</h1>
+    <div className="font-cousine w-[100%] h-[100vh] bg-blue-400 text-xl text-white items-center flex justify-center">
+      <div className='bg-gray-700 w-[50%] h-[50vh] flex items-center justify-center'>
+      <p className="text-3xl text-bold">Shareify</p>
       <p>Share your files with just a link!</p>
       </div>
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cousine': ['Cousine', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
-
