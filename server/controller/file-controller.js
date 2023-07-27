526d8ff5c438f4842b38e83ff82c56cfe7430f73
agamswaroop619 <agam.swaroop619@gmail.com>
2023-07-27 22:48:15 +0530

    Local server working (file transfer possible)

--- server/controller/file-controller.js
+++ server/controller/file-controller.js
@@ -1,12 +1,15 @@
-import File from "../models/file";
-export const uploadFiles = (request, response) =>{
+import File from "../models/file.js";
+export const uploadFiles = async(request, response) =>{
    const fileObj ={
     path: request.file.path,
     name: request.file.originalname
    }
     try {
-    
+        const file= await File.create(fileObj);
+        console.log(file);
+        response.status(200).json({path: `http://localhost:8000/file/${file._id}` })
    } catch (error) {
     console.error(error.message);
+    response. status (500).json({error: error.message });
    }
 }
